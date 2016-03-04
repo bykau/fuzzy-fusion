0c8dfff1fed03e1572bd0ae75396c0273c8d88b5
Evgeny <krivosheevevgeny@gmail.com>
2016-03-03 21:00:13 -0500

    feature/ update em/ add data

--- src/algorithm/em.py
+++ src/algorithm/em.py
@@ -7,6 +7,7 @@ Data Fusion: Resolvnig Conflicts from Multiple Sources
 '''
 
 import numpy as np
+import pandas as pd
 
 s_number = 5
 max_rounds = 30
@@ -15,12 +16,9 @@ eps = 0.001
 
 def get_n_params(data):
     n_list = []
-    for i in range(len(data[0])):
-        observed_data = [obj[i] for obj in data]
-        if None in observed_data:
-            n = len(set(observed_data))-2
-        else:
-            n = len(set(observed_data))-1
+    obj_list = sorted(data.O.drop_duplicates())
+    for i in obj_list:
+        n = len(data[data.O == i].V.drop_duplicates()) - 1
         n_list.append(n)
     return n_list
 
@@ -31,13 +29,14 @@ def get_accuracy(data, prob):
     for s_index in range(s_number):
         p_sum = 0.
         size = 0.
-        for obj_index in range(len(data[0])):
-            observed_val = data[s_index][obj_index]
-            if not observed_val:
+        for obj_index in range(len(data.O.drop_duplicates())):
+            observed_val = list(data[(data.S == s_index) & (data.O == obj_index)].V)
+            if len(observed_val) != 0:
+                observed_val == observed_val[0]
+            else:
                 continue
             size += 1
-            observed_values = sorted([obj[obj_index] for obj in data])
-            possible_values = sorted(list(set(observed_values)-set([None])))
+            possible_values = sorted(list(set(data[data.O == obj_index].V)))
             values.append(possible_values)
             for v_ind, v in enumerate(possible_values):
                 if v == observed_val:
@@ -45,40 +44,27 @@ def get_accuracy(data, prob):
                     break
         accuracy = p_sum/size
         accuracy_list.append(accuracy)
-    return accuracy_list, values
+    return accuracy_list
 
 
 def get_prob(data, accuracy):
-    n_list = get_n_params(data)
+    n_list = get_n_params(data=data)
     likelihood = []
     for obj_index in range(len(truth_obj_list)):
             likelihood.append([])
             n = n_list[obj_index]
-            observed_values = sorted([obj[obj_index] for obj in data])
-            possible_values = sorted(list(set(observed_values)-set([None])))
+            observed_values = list(data[data.O == obj_index].V)
+            possible_values = sorted(list(set(observed_values)))
             if n == 0:
                 likelihood[obj_index].append(1.)
                 continue
-            # break_flag = False
             for v_true in possible_values:
                 a, b, b_sum = 1., 1., 0.
                 a_not_completed = True
                 for v_possible in possible_values:
-                    # if break_flag:
-                    #     break
-                    for v, s_index in zip(observed_values, range(s_number)):
-                        if v == None:
-                            continue
-                        accuracy = accuracy_list[s_index]
-                        # if accuracy == 1.:
-                            # if v == v_true:
-                            #     likelihood[obj_index].append(1.)
-                            #     break_flag = True
-                            #     break
-                            # else:
-                            #     likelihood[obj_index].append(0.)
-                            #     break_flag = True
-                            #     break
+                    for inst in data[data.O == obj_index].iterrows():
+                        accuracy = accuracy_list[inst[1].S]
+                        v = inst[1].V
                         if v == v_possible:
                             b *= n*accuracy/(1-accuracy)
                         if a_not_completed and v == v_true:
@@ -86,26 +72,26 @@ def get_prob(data, accuracy):
                     a_not_completed = False
                     b_sum += b
                     b = 1
-                # if break_flag:
-                #     break_flag = False
-                #     continue
                 p = a/b_sum
                 likelihood[obj_index].append(p)
     return likelihood
 
 
-def get_dist_metric(data, prob):
+def get_gt_prob(data, truth_obj_list):
     prob_gt = []
-    for obj_index in range(len(data[0])):
-        observed_values = sorted([obj[obj_index] for obj in data])
-        possible_values = sorted(list(set(observed_values)-set([None])))
-        prob_gt.append(possible_values)
+    val = []
+    for obj_index in range(len(data.O.drop_duplicates())):
+        possible_values = sorted(list(set(data[data.O == obj_index].V)))
+        val.append(possible_values)
+        prob_gt.append([0]*len(possible_values))
     for obj_ind, v_true in enumerate(truth_obj_list):
-        for v_ind, v in enumerate(prob_gt[obj_ind]):
+        for v_ind, v in enumerate(val[obj_ind]):
             if v == v_true:
                 prob_gt[obj_ind][v_ind] = 1
-            else:
-                prob_gt[obj_ind][v_ind] = 0
+    return prob_gt, val
+
+
+def get_dist_metric(prob_gt, prob):
     prob_gt_vector = []
     prob_vector = []
     for i in range(len(prob_gt)):
@@ -116,33 +102,26 @@ def get_dist_metric(data, prob):
 
 
 if __name__ == '__main__':
+    data = pd.read_csv('../../data/observation.csv', names=['S', 'O', 'V'])
     truth_obj_list = [6, 8, 9, 15, 16, 10, 11, 7, 18, 20]
-    data = [
-        [6, 19, None, 15, None, 16, 11, 5, 18, 20],
-        [6, None, 9, 15, 16, 22, 21, None, None, 20],
-        [None, 8, 23, 15, None, 10, 11, 7, None, 24],
-        [13, None, 9, 15, None, 10, 11, 7, 18, None],
-        [None, 8, 9, 15, 16, 28, 12, None, None, 20]
-    ]
 
-
-    dist_metric_list = []
-    iter_number_list = []
     accuracy_delta = 0.3
     iter_number = 0
     accuracy_list = [0.8]*s_number
     while accuracy_delta > eps and iter_number < max_rounds:
         prob = get_prob(data=data, accuracy=accuracy_list)
         accuracy_prev = accuracy_list
-        accuracy_list, possible_values = get_accuracy(data, prob)
+        accuracy_list = get_accuracy(data=data, prob=prob)
         accuracy_delta = max([abs(k-l) for k, l in zip(accuracy_prev, accuracy_list)])
         iter_number += 1
-    dist_metric = get_dist_metric(data, prob)
+
+    prob_gt, val = get_gt_prob(data=data, truth_obj_list=truth_obj_list)
+    dist_metric = get_dist_metric(prob_gt=prob_gt, prob=prob)
 
     print 'max_dist_metr: {}'.format(dist_metric)
     print 'iter number: {}'.format(iter_number)
     print '------------'
-    for v, p in zip(possible_values, prob):
+    for v, p in zip(val, prob):
         print v
         print p
         print '_____'
