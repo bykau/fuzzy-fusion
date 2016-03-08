ee911b87a0a2c193ac02ba532219b631403d33f6
Evgeny <krivosheevevgeny@gmail.com>
2016-03-08 10:55:56 -0500

    feature/generator/ g_frame

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -3,6 +3,7 @@ import random
 
 
 def generator(cov_list, p_list, ground_truth, claster_size):
+    number_of_obj = len(ground_truth)
     source_number = 3
     g = 1./(claster_size+1)
     g_true = g*2
@@ -11,7 +12,7 @@ def generator(cov_list, p_list, ground_truth, claster_size):
         p_s = p_list[s]
         cov_s = cov_list[s]
         flag = False
-        for obj_ind in range(len(ground_truth)):
+        for obj_ind in range(number_of_obj):
             true_val = ground_truth[obj_ind]
             if random.uniform(0, 1) <= cov_s:
                 if obj_ind < claster_size:
@@ -37,6 +38,18 @@ def generator(cov_list, p_list, ground_truth, claster_size):
                     else:
                         val = 1-true_val
                 data.append([s, obj_ind, val])
-    data_frame = pd.DataFrame(data=data, columns=['S', 'O', 'V'])
-    return data_frame
 
+    g_list = []
+    for o_i in range(number_of_obj):
+        if o_i < claster_size:
+            for o_j in range(claster_size):
+                if o_i == o_j:
+                    g_list.append([o_i, o_j, g_true])
+                else:
+                    g_list.append([o_i, o_j, g])
+        else:
+            g_list.append([o_i, o_i, 1.])
+
+    g_frame = pd.DataFrame(g_list, columns=['Oi', 'Oj', 'P'])
+    data_frame = pd.DataFrame(data=data, columns=['S', 'O', 'V'])
+    return [data_frame, g_frame]
