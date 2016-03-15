126776e2cf514b95b2dbb8ffd1d88c4133818087
Evgeny <krivosheevevgeny@gmail.com>
2016-03-15 16:03:23 -0400

    feature/generator/ change generation rules

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -2,43 +2,42 @@ import pandas as pd
 import random
 
 
-def generator(cov_list, p_list, ground_truth, claster_size):
+def generator(cov_list, p_list, ground_truth, claster_size, g_true):
     number_of_obj = len(ground_truth)
-    source_number = 3
-    g = 1./(claster_size+1)
-    g_true = g*2
+    source_number = len(p_list)
+    g = (1.-g_true)/(claster_size-1)
     data = []
     for s in range(source_number):
         p_s = p_list[s]
         cov_s = cov_list[s]
-        flag = False
+        prohib_ind = []
         for obj_ind in range(number_of_obj):
             true_val = ground_truth[obj_ind]
             if random.uniform(0, 1) <= cov_s:
                 if obj_ind < claster_size:
-                    if flag:
+                    if obj_ind in prohib_ind:
                         continue
-                    if random.uniform(0, 1) <= g_true:
+                    prohib_ind.append(obj_ind)
+                    if random.uniform(0, 1) <= g_true or len(prohib_ind) == claster_size:
                         if random.uniform(0, 1) <= p_s:
                             val = ground_truth[s]
                         else:
                             val = 1-ground_truth[s]
                     else:
                         claster_ind = range(claster_size)
-                        claster_ind.remove(s)
+                        claster_ind = list(set(claster_ind)^set(prohib_ind))
                         swp_index = random.choice(claster_ind)
                         if random.uniform(0, 1) <= p_s:
                             val = ground_truth[swp_index]
                         else:
                             val = 1-ground_truth[swp_index]
-                    flag = True
-                    data.append([s, s, val])
+                        prohib_ind.append(swp_index)
                 else:
                     if random.uniform(0, 1) <= p_s:
                         val = true_val
                     else:
                         val = 1-true_val
-                    data.append([s, obj_ind, val])
+                data.append([s, obj_ind, val])
 
     g_list = []
     for o_i in range(number_of_obj):
