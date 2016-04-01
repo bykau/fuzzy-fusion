d91ec14f672f3d2d593d251ac671195ab9aa3f61
Evgeny <krivosheevevgeny@gmail.com>
2016-03-31 22:07:27 -0400

    mvalues/adapt generator

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import random
+import copy
 
 
-def generator(cov_list, p_list, ground_truth, claster_size, g_true):
+def generator(cov_list, p_list, ground_truth, claster_size, g_true, values):
     number_of_obj = len(ground_truth)
     source_number = len(p_list)
     if claster_size-1 != 0:
@@ -15,38 +16,39 @@ def generator(cov_list, p_list, ground_truth, claster_size, g_true):
             cov_s = cov_list[s]
             for obj_ind in range(number_of_obj):
                 true_val = ground_truth[obj_ind]
+                p_val = copy.copy(values)
                 if random.uniform(0, 1) <= cov_s:
-                    if obj_ind < claster_size:
-                        if random.uniform(0, 1) <= g_true:
-                            if random.uniform(0, 1) <= p_s:
-                                val = true_val
-                            else:
-                                val = 1-true_val
-                        else:
-                            claster_ind = range(claster_size)
-                            claster_ind.remove(obj_ind)
-                            swp_index = claster_ind[random.randint(0, len(claster_ind)-1)]
-                            if random.uniform(0, 1) <= p_s:
-                                val = ground_truth[swp_index]
-                            else:
-                                val = 1-ground_truth[swp_index]
-                    else:
+                    if random.uniform(0, 1) <= g_true:
                         if random.uniform(0, 1) <= p_s:
                             val = true_val
                         else:
-                            val = 1-true_val
+                            p_val.remove(true_val)
+                            val = random.choice(p_val)
+                    else:
+                        if obj_ind % 2 == 0:
+                            swp_ind = obj_ind + 1
+                        else:
+                            swp_ind = obj_ind - 1
+                        if random.uniform(0, 1) <= p_s:
+                            val = ground_truth[swp_ind]
+                        else:
+                            p_val.remove(ground_truth[swp_ind])
+                            val = random.choice(p_val)
+
                     data.append([s, obj_ind, val])
 
         g_list = []
         for o_i in range(number_of_obj):
-            if o_i < claster_size:
-                for o_j in range(claster_size):
-                    if o_i == o_j:
-                        g_list.append([o_i, o_j, g_true])
-                    else:
-                        g_list.append([o_i, o_j, g])
+            if o_i % 2 == 0:
+                swp_ind = o_i + 1
             else:
-                g_list.append([o_i, o_i, 1.])
+                swp_ind = o_i - 1
+
+            for o_j in [o_i, swp_ind]:
+                if o_i == o_j:
+                    g_list.append([o_i, o_j, g_true])
+                else:
+                    g_list.append([o_i, o_j, g])
 
         g_frame = pd.DataFrame(g_list, columns=['Oi', 'Oj', 'P'])
         data_frame = pd.DataFrame(data=data, columns=['S', 'O', 'V'])
