4611518e3a03a0d8698716316957eb08f44207f0
Evgeny <krivosheevevgeny@gmail.com>
2016-04-01 19:53:22 -0400

    mvalues/em/transfer

--- src/algorithm/em.py
+++ src/algorithm/em.py
@@ -13,11 +13,12 @@ max_rounds = 30
 eps = 0.05
 
 
-def get_init_prob(data):
+def init_prob(data, values):
     init_prob = []
+    l = len(values)
     obj_index_list = sorted(data.O.drop_duplicates())
     for obj_index in obj_index_list:
-        init_prob.append([0.5, 0.5])
+        init_prob.append([1./l]*l)
     return init_prob
 
 
@@ -39,40 +40,45 @@ def get_accuracy(data, prob, s_number):
     return accuracy_list
 
 
-def get_factor(data, accuracy, v, v_true):
+def get_factor(accuracy, v, v_true, n):
     if v == v_true:
         factor = accuracy
     else:
-        factor = (1 - accuracy)
+        factor = (1 - accuracy)/n
     return factor
 
 
-def get_prob(data, truth_obj_list, accuracy_list):
+def get_prob(data, truth_obj_list, accuracy_list, values):
     likelihood = []
-    possible_values = [0, 1]
+    l = len(values)
+    n = l - 1
     for obj_index in range(len(truth_obj_list)):
-        a, b = 1., 1.
+        prob = []
+        term_list = [1]*l
         for inst in data[data.O == obj_index].iterrows():
             accuracy = accuracy_list[inst[1].S]
             v = inst[1].V
-            a *= get_factor(data, accuracy, v, possible_values[0])
-            b *= get_factor(data, accuracy, v, possible_values[1])
-        likelihood.append([a/(a+b), b/(a+b)])
+            for v_ind, v_true in enumerate(values):
+                term_list[v_ind] *= get_factor(accuracy, v, v_true, n)
+        denom = sum(term_list)
+        for v_ind in range(l):
+            prob.append(term_list[v_ind]/denom)
+        likelihood.append(prob)
     return likelihood
 
 
-def get_gt_prob(data, truth_obj_list):
+def get_gt_prob(data, truth_obj_list, values):
     prob_gt = []
     val = []
+    l = len(values)
     for obj_index in range(len(data.O.drop_duplicates())):
-        possible_values = [0, 1]
-        val.append(possible_values)
-        prob_gt.append([0]*len(possible_values))
+        val.append(values)
+        prob_gt.append([0]*l)
     for obj_ind, v_true in enumerate(truth_obj_list):
         for v_ind, v in enumerate(val[obj_ind]):
             if v == v_true:
                 prob_gt[obj_ind][v_ind] = 1
-    return prob_gt, val
+    return prob_gt
 
 
 def get_dist_metric(prob_gt, prob):
@@ -86,19 +92,19 @@ def get_dist_metric(prob_gt, prob):
     return dist_metric_norm
 
 
-def em(data, accuracy, truth_obj_list):
-    prob = get_init_prob(data=data)
+def em(data, accuracy, truth_obj_list, values):
+    prob = init_prob(data=data, values=values)
     s_number = len(accuracy.S)
     accuracy_list = list(accuracy.A)
     accuracy_delta = 0.3
     iter_number = 0
     while accuracy_delta > eps and iter_number < max_rounds:
-        prob = get_prob(data=data, truth_obj_list=truth_obj_list, accuracy_list=accuracy_list)
+        prob = get_prob(data=data, truth_obj_list=truth_obj_list, accuracy_list=accuracy_list, values=values)
         accuracy_prev = copy.copy(accuracy_list)
         accuracy_list = get_accuracy(data=data, prob=prob, s_number=s_number)
         accuracy_delta = max([abs(k-l) for k, l in zip(accuracy_prev, accuracy_list)])
         iter_number += 1
 
-    prob_gt, val = get_gt_prob(data=data, truth_obj_list=truth_obj_list)
+    prob_gt = get_gt_prob(data=data, truth_obj_list=truth_obj_list, values=values)
     dist_metric = get_dist_metric(prob_gt=prob_gt, prob=prob)
     return [dist_metric, iter_number]
