ea3e932396495b29861f1177af6b6cd1bb9dfc12
Evgeny <krivosheevevgeny@gmail.com>
2016-05-11 22:39:27 -0400

    fix/flights/add/data

--- experiment/flights/flights.py
+++ experiment/flights/flights.py
@@ -7,6 +7,7 @@ path_to_raw_data = '../../data/flight/raw_data/data.txt'
 def cook_data():
     raw_gt = pd.read_csv(path_to_raw_gt, sep='\t', names=['O', 'V'], index_col=False).drop_duplicates()
     gt_data = raw_gt[raw_gt.V.notnull()]
+    gt_data = gt_data.ix[gt_data.O.drop_duplicates().index]
     gt_data.to_csv('../../data/flight/data/gt.csv', index=False)
 
     data_list = []
@@ -17,7 +18,10 @@ def cook_data():
         votes = raw_votes[raw_votes.notnull()]
         for vote in votes.iteritems():
             s = vote[0]
-            val = vote[1]
+            try:
+                val = int(vote[1])
+            except ValueError:
+                val = vote[1]
             data_list.append([s, obj_ind, val])
     data = pd.DataFrame(data=data_list, columns=['S', 'O', 'V'])
     data.to_csv('../../data/flight/data/data.csv', index=False)
